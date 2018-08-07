9294e1e3ec2fd7f81ca6b93296fd0b43409502af
Armando Arce <armando.arce@gmail.com>
2018-08-07 11:42:55 -0600

    First test

--- src/main.lua
+++ src/main.lua
@@ -1,12 +1,15 @@
-	
-local sqlite3 = require("lsqlite3")
-local db = sqlite3.open_memory()
-db:exec[[
-  CREATE TABLE test (id INTEGER PRIMARY KEY, content);
-  INSERT INTO test VALUES (NULL, 'Hello World');
-  INSERT INTO test VALUES (NULL, 'Hello Lua');
-  INSERT INTO test VALUES (NULL, 'Hello Sqlite3')
-]]
-for row in db:nrows("SELECT * FROM test") do
-  print(row.id, row.content)
+local img
+function setup()
+  size(320,240)
+  local sqlite3 = require("lsqlite3")
+  local db = sqlite3.open('world_countries.mbtiles')
+  local vm = db:prepare('select tile_data from images where tile_id=37')
+  while (vm:step() == sqlite3.ROW) do
+    data = vm:get_uvalues()
+  end
+  img = importImage(data,string.len(data))
+end
+function draw()
+  background(255)
+  image(img,0,0)
 end
